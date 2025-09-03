1d30563e780803d79c59198ce6d98341a95c378d
Amr <amr20012200131@hotmail.com>
2025-09-03 13:25:03 +0300

    Adding comment to LoopChallange

--- JS/LoopChallange/LoopChallange.js
+++ JS/LoopChallange/LoopChallange.js
@@ -1,30 +1,31 @@
+
+//Display ODD numbers
 for(var i=1;i<=20;i++){
     if(i%2!==0) {
-        console.log(i);
-        
+        console.log(i);  
     }
 }
 
+//Decreasing multiples of 3
 for (var i=100;i>=0;i--) {
     if (i%3==0){
         console.log(i);
-
-        
     }
-
 }
 
+//Print the given sequence
 for(var i=4;i>=-3.5;i-=1.5){
     console.log(i);
-
 }
+
+//Sigma
 var sum=0
 for(var i=1;i<=100;i++){
     sum =sum+i
 }
-
 console.log(sum);
 
+//Factorial
 var fac=1
 for(var i=1;i<=12;i++){
     fac =fac*i
