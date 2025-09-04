0db06d293ec4dce7fe49aa10ef5d1d00584430ab
Amr <amr20012200131@hotmail.com>
2025-09-04 16:11:13 +0300

    Added Likes

---
+++ JS/Likes/script.js
@@ -0,0 +1,14 @@
+function neilLike() {
+    let like = document.getElementById("neil-like")
+    like.innerText = Number(like.innerText) + 1
+}
+
+function nicholelike() {
+    let like = document.getElementById("nichole-like")
+    like.innerText = Number(like.innerText) + 1
+}
+
+function jimLike() {
+    let like = document.getElementById("jim-like")
+    like.innerText = Number(like.innerText) + 1
+}
