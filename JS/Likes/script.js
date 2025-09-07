d5c9f658a09a49b5b988372daad5d6a69fe36bb9
Amr <amr20012200131@hotmail.com>
2025-09-07 09:11:21 +0300

    changed it to one function

--- JS/Likes/script.js
+++ JS/Likes/script.js
@@ -1,14 +1,13 @@
-function neilLike() {
-    let like = document.getElementById("neil-like")
-    like.innerText = Number(like.innerText) + 1
-}
-
-function nicholelike() {
-    let like = document.getElementById("nichole-like")
-    like.innerText = Number(like.innerText) + 1
-}
-
-function jimLike() {
-    let like = document.getElementById("jim-like")
-    like.innerText = Number(like.innerText) + 1
+function likes(el) {
+    let id = el.id;
+    if (id === "nichole-btn") {
+        let like = document.getElementById("nichole-like")
+        like.innerText = Number(like.innerText) + 1
+    } else if (id === "neil-btn") {
+        let like = document.getElementById("neil-like")
+        like.innerText = Number(like.innerText) + 1
+    } else {
+        let like = document.getElementById("jim-like")
+        like.innerText = Number(like.innerText) + 1
+    }
 }
