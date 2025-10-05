5e583d7ac239564bde2ba78253f89f0e9753f778
Amr <amr20012200131@hotmail.com>
2025-10-05 16:05:22 +0300

    added Ninja gold

--- Python/python_stack/django/django_fundamentals/ninja_gold/myApp/views.py
+++ Python/python_stack/django/django_fundamentals/ninja_gold/myApp/views.py
@@ -18,21 +18,21 @@ def gold( request ):
     time = strftime(" %B %d %Y %H:%M %p", gmtime())
     num = random.randint(10, 20)
     if "farm" in request.POST:
-        request.session["activity"].append(f"you entered a farm and earned {num} gold. ({time})")
+        request.session["activity"].insert(0,f"you entered a farm and earned {num} gold. ({time})")
         context["num"] += num
     elif "cave" in request.POST:
-        request.session['activity'].append(f"you entered a cave and earned {num} gold. ({time})")
+        request.session['activity'].insert(0,f"you entered a cave and earned {num} gold. ({time})")
         context["num"] += num
     elif "house" in request.POST:
-        request.session['activity'].append(f"you entered a house and earned {num} gold. ({time})")
+        request.session['activity'].insert(0,f"you entered a house and earned {num} gold. ({time})")
         context["num"] += num
     else:
         num = random.randint(0, 50)
         win = random.randint(0, 1)
         if win == 1:
-            request.session['activity'].append(f"you completed a quest and earned {num} gold. ({time})")
+            request.session['activity'].insert(0,f"you completed a quest and earned {num} gold. ({time})")
         else:
-            request.session['activity'].append(f"you failed a quest and lost {num} gold Ouch. ({time})")
+            request.session['activity'].insert(0,f"you failed a quest and lost {num} gold Ouch. ({time})")
             num *= -1
         context["num"] += num
 
