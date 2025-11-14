c38557ea83371c7f16afe1e7018143c50b5d4c9c
Amr <amr20012200131@hotmail.com>
2025-11-14 14:18:24 +0200

    updated RenderingBook

--- Java/SpringBootAssignments/RenderingBooks/src/main/java/com/example/renderingbooks/Controllers/BooksApi.java
+++ Java/SpringBootAssignments/RenderingBooks/src/main/java/com/example/renderingbooks/Controllers/BooksApi.java
@@ -3,10 +3,7 @@ package com.example.renderingbooks.Controllers;
 
 import com.example.renderingbooks.Models.Book;
 import com.example.renderingbooks.Services.BookService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BooksApi {
@@ -26,8 +23,8 @@ public class BooksApi {
         bookService.createBook(book2);
         return "Book created";
     }
-    // other methods removed for brevity
-    @RequestMapping("/api/books/{id}")
+
+    @PutMapping("/api/books/{id}")
     public Book update(
             @PathVariable("id") Long id,
             @RequestParam(value="title") String title,
@@ -38,7 +35,7 @@ public class BooksApi {
         return bookService.updateBook(id, title, desc, lang, numOfPages);
     }
 
-    @RequestMapping("/delete/books/{id}")
+    @DeleteMapping("/delete/books/{id}")
     public void destroy(@PathVariable("id") Long id) {
         bookService.deleteBook(id);
     }
