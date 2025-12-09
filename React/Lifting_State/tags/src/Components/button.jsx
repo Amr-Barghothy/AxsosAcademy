f76c3602e8ad43b5516ca0e42825c11adca219cc
Amr <amr20012200131@hotmail.com>
2025-12-09 10:41:47 +0200

    uploaded tags

--- React/Lifting_State/tags/src/Components/button.jsx
+++ React/Lifting_State/tags/src/Components/button.jsx
@@ -6,12 +6,12 @@ export default function Button({number, page, setPage, arr}) {
         <>
             <button
                 onClick={() => setPage(number)}
-                className={`border-1 p-10 ${isActive ? "bg-black text-white" : "bg-white text-black"} `}>
+                className={`border-1 p-10 ${isActive ? "bg-black text-white transition-all duration-500" : "bg-white text-black"} `}>
                 Tab {number}
             </button>
             {isActive && (
                 <textarea name="content" id="content" cols="70" rows="10" value={arr}
-                          className={"absolute top-1/2 translate-x-auto -translate-y-1/2 flex border-1 overflow-auto"}>
+                          className={"absolute top-1/2 translate-x-auto -translate-y-1/2 flex border-1 overflow-auto transition-all duration-500"}>
                 </textarea>
             )}
         </>
