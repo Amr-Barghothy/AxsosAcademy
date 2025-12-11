744c1a8d7414a281358ec9cae7a41d77488ab765
Amr <amr20012200131@hotmail.com>
2025-12-11 14:49:16 +0200

    uploaded Faker Api

---
+++ MERN/React_Routing/routing-practice/uno.config.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const unocss_1 = require("unocss");
+exports.default = (0, unocss_1.defineConfig)({
+// ...UnoCSS options
+});
